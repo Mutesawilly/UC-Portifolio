b4d434a44f1127924929fd46c78d82634e8cee46
Mutesa Willy <mutesawilly682@gmail.com>
2025-10-03 21:31:19 +0200

    Changes

--- app/page.js
+++ app/page.js
@@ -14,31 +14,42 @@ export default function Home() {
           </div>
         </nav>
         {/* The Biography and Personal Description */}
-        <div className="flex flex-col items-center justify-center">
-          <Image src="/images.jpg" alt="Profile picture" width={180} height={100} className="cursor-pointer" />
+        <div className="relative flex flex-col items-center justify-center">
+          <div className="group flex items-center justify-center">
+            <Image src="/images.jpg" alt="Profile picture" width={180} height={100} className="cursor-pointer hover:rotate-z-15 ease-in duration-100" />
+            <p className="absolute top-65 rotate-45 left-140 text-4xl hidden group-hover:block">⋆｡‧˚ʚ🍫ɞ˚‧｡⋆</p>
+            <p className="absolute top-5 rotate-400 right-135 text-4xl hidden group-hover:block">༄˖°.☕️.ೃ࿔📚*:･</p>
+          </div>
           <div className="flex flex-col items-center w-[60%] text-center">
             <h1 className="font-[girl-next-door] text-5xl py-7">About Me</h1>
             <p className="font-sans font-medium text-lg">I&#39;m Christella, a Data Scientist & Insight Alchemist with a background in tech, healthcare, and finance who crafts raw information into actionable intelligence you can leverage. Since 2019, I have focused on translating complexity into intuitive, data-driven solutions.</p>
+
           </div>
         </div>
       </div>
 
-      {/* Transition Sentence From Biography To Activities */}
-      <div className="flex items-center justify-center pb-20">
-        <div className="group relative flex-col justify-between pt-50px">
-          <p className="absolute sm:left-[-50px] md:left-[-100px] text-left text-4xl">⋆｡‧˚ʚ🍫ɞ˚‧｡⋆</p>
-          <p className="absolute sm:right-[-100px] md:right-[-200px] sm:bottom-[-50px] md:bottom-[-50px] text-right text-4xl">༄˖°.☕️.ೃ࿔📚*:･</p>
-          <p className="py-5 text-3xl sm:pt-15 md:pt-17">If not <span className="text-pink-600 cursor-pointer">me, who?</span> If not <span className="text-green-600 cursor-pointer">now, when?</span></p>
+
+      {/* Selected Projects Page
+      <div className="flex flex-wrap items-center justify-center p-10 gap-5">
+        <div className="bg-pink-100 p-5 w-[68%]">
+          <p>Helping users track their mental well-being through quick daily check-ins, built to feel warm, intuitive, and stigma-free.</p>
         </div>
-      </div>
+        <div className="bg-pink-50 w-[30%] p-5">
+          <p>A dashboard that unifies orders, sales, and returns across Amazon, Flipkart, and Myntra in one place.</p>
+        </div>
+      </div> */}
 
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center p-2">
         <div className="bg-pink-100 w-dvw flex justify-center p-10 rounded-lg">
           <div className="flex flex-col items-center">
             <p className="flex items-center justify-center w-15 h-15 rounded-full bg-pink-300 text-2xl cursor-pointer text-pink-800 font-bold">UC</p>
             <p className="font-medium text-[60px] pt-10 pb-5">Let&#39;s get down to serious business</p>
-            <p className="text-2xl">let’s talk design, internet culture, or your latest playlist.</p>
-            <div className="flex items-center justify-center gap-10 pt-15">
+
+            {/* Transition Sentence From Biography To Activities */}
+            <div className="flex items-center justify-center pb-10">
+              <p className="text-3xl">If not <span className="text-pink-600 cursor-pointer">me, who?</span> If not <span className="text-green-600 cursor-pointer">now, when?</span></p>
+            </div>
+            <div className="flex items-center justify-center gap-10">
               <div className="bg-pink-200 px-8 py-3 rounded-4xl text-xl flex items-center justify-center gap-2 hover:bg-pink-300 ease-in-out duration-500 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" height={25} width={25}>
                   <path d="M112 128C85.5 128 64 149.5 64 176C64 191.1 71.1 205.3 83.2 214.4L291.2 370.4C308.3 383.2 331.7 383.2 348.8 370.4L556.8 214.4C568.9 205.3 576 191.1 576 176C576 149.5 554.5 128 528 128L112 128zM64 260L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 260L377.6 408.8C343.5 434.4 296.5 434.4 262.4 408.8L64 260z" fill="#323544" />
